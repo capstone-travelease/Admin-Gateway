d4a54930dde3b847b58931518060f26994ed078e
billzayy <coderbillzay@gmail.com>
2024-04-08 17:13:04 +0700

    refactor: Add final CORS in Gateway

--- src/main/java/com/capstone/GatewayAdmin/GatewayAdminApplication.java
+++ src/main/java/com/capstone/GatewayAdmin/GatewayAdminApplication.java
@@ -1,7 +1,13 @@
 package com.capstone.GatewayAdmin;
 
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class GatewayAdminApplication {
@@ -9,5 +15,16 @@ public class GatewayAdminApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayAdminApplication.class, args);
 	}
-
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		// Use allowedOriginPatterns to allow multiple origins
+		config.setAllowedOriginPatterns(Arrays.asList("*"));
+		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "responseType", "Authorization"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter();
+	}
 }

6e9b244abd0f69aa32b539f2987149a3aa938cac
billzayy <coderbillzay@gmail.com>
2024-04-09 11:38:24 +0700

    fix: change PUT method to POST method

--- src/main/java/com/capstone/GatewayAdmin/GatewayAdminApplication.java
+++ src/main/java/com/capstone/GatewayAdmin/GatewayAdminApplication.java
@@ -1,13 +1,13 @@
 package com.capstone.GatewayAdmin;
 
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class GatewayAdminApplication {
@@ -15,16 +15,17 @@ public class GatewayAdminApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayAdminApplication.class, args);
 	}
-	@Bean
-	public CorsFilter corsFilter() {
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		final CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		// Use allowedOriginPatterns to allow multiple origins
-		config.setAllowedOriginPatterns(Arrays.asList("*"));
-		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "responseType", "Authorization"));
-		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
-		source.registerCorsConfiguration("/**", config);
-		return new CorsFilter();
-	}
+
+//	@Bean
+//	public CorsConfigurationSource corsConfigurationSource() {
+//		final CorsConfiguration configuration = new CorsConfiguration();
+//		configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+//		configuration.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+//		configuration.setAllowCredentials(true);
+//		configuration.setAllowedHeaders(List.of("*"));
+//		configuration.setExposedHeaders(List.of("X-Auth-Token","Authorization","Access-Control-Allow-Origin","Access-Control-Allow-Credentials"));
+//		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		source.registerCorsConfiguration("/**", configuration);
+//		return source;
+//	}
 }
